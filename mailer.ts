ea9ef922975bc91ae76d2c14f211f0679c911499
Max Tang <maxtang@glosfer.com>
2018-04-09 10:37:12 +0900

    aysnc bug

--- mailer.ts
+++ mailer.ts
@@ -1,5 +1,6 @@
 import {mailConfig} from './mailConfig'
 import {confirmMail} from './email'
+import { isUndefined } from 'util';
 const express = require('express')
 const nodemailer = require('nodemailer')
 const bodyParser = require('body-parser')
@@ -9,18 +10,27 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+const queue = []
+
 app.post('/mailer', function(req, res){
     let name = req.body.name.trim()
     let email = req.body.email.trim()
     if(email){
         console.log(name,': your email has been sent to: ',email)
-        setTimeout(() => doMail(email), 200)
+        queue.push(email)
     }
     else{
         console.log('fail to get email address')
     }
     res.end()
 })
+
+setInterval(() => {
+    let email = queue.splice(0,1)
+    if(email.length > 0) {
+        doMail(email[0])
+    }
+}, 200)
 
 app.listen(3000, function(){
     console.log('server is lestening on localhost:3000')
